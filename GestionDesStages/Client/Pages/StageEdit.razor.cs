31998701aa61e8900bd69107d0eab20996f755f1
MarlondAugustin <2043906@cmontmorency.qc.ca>
2022-09-26 17:10:06 -0400

    Commit vidéo 14

---
+++ GestionDesStages/Client/Pages/StageEdit.razor.cs
@@ -0,0 +1,32 @@
+﻿using GestionDesStages.Shared.Models;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components;
+
+namespace GestionDesStages.Client.Pages
+{
+    public partial class StageEdit
+    {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        public AuthenticationStateProvider GetAuthenticationStateAsync { get; set; }
+
+        public Stage Stage { get; set; } = new Stage();
+
+
+
+        protected async Task HandleValidSubmit()
+        {
+        }
+
+        protected void HandleInvalidSubmit()
+        {
+        }
+
+        protected void NavigateToOverview()
+        {
+            NavigationManager.NavigateTo("/");
+        }
+    }
+}
